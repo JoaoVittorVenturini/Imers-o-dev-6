d7f7d3a12acbdd083f27875213f5c4785fb66a83
João Vittor Venturini <joaovittorventurini@outlook.com>
2023-02-01 10:04:19 -0300

    aula 02

---
+++ Aula01/main.js
@@ -0,0 +1,6 @@
+var valorEmDolar = 400;
+var catacaoDolar = 5.09;
+
+var valorEmReal = valorEmDolar * catacaoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+alert("O valor em real é R$ " + valorEmReal);
---
+++ Desafios/02/main.js
@@ -0,0 +1,30 @@
+const valor = document.querySelector('.container input');
+const resposta = document.querySelector('.resultado');
+const tentativa = document.querySelector('.n-tentativa');
+const possibilidades = document.querySelector('.n-possibilidades');
+const btn = document.querySelector('.btn');
+
+var secretNumber = parseInt(Math.random() * 1001);
+
+const answer = () => {
+  // verificar se o valor é válido (entre 1 e 1000)
+  if (valor.value <= 0 || valor.value > 1000) {
+    document.querySelector('.resultado').innerText = "Digite um número válido";
+  } else {
+    if(valor.value == secretNumber) {
+      document.querySelector('.resultado').innerText = "Parabéns! Você acertou!";
+    } else if( valor.value > secretNumber) {
+      document.querySelector('.resultado').innerText = "O número secreto é menor";
+    } else {
+      document.querySelector('.resultado').innerText = "O número secreto é maior";
+    }
+    
+    tentativa.innerText = parseInt(tentativa.innerText) + 1;
+    if(tentativa.innerText == 10) {
+      alert("Você atingiu o limite de tentativas! " + " O número secreto era " + secretNumber);
+      btn.disabled = true;
+    }
+  }
+}
+
+btn.addEventListener("click", answer);
